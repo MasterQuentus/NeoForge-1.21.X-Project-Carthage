7ea35c47c17a8415a16d02056377f66725c04584
MasterQuentus <daltonrpratt@gmail.com>
2025-08-19 15:04:18 -0500

    changed Some things

---
+++ src/main/java/com/quentus/projectcarthage/ProjectCarthage.java
@@ -0,0 +1,46 @@
+package com.quentus.projectcarthage;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(ProjectCarthage.MOD_ID)
+public class ProjectCarthage {
+    public static final String MOD_ID = "projectcarthage";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public ProjectCarthage(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
